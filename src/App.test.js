62b21df3d79f8c7294678849699afa1fbc2dc414
lycia <lycianex27@gmail.com>
2023-11-28 13:45:54 +0200

    structure

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Form from "./Form";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import "./style.css";
+import Cities from "./Cities";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <div className="App">
+      <h1>Search Engine</h1>
+      <h2>
+        <Cities />
+      </h2>
+      <Form />
+    </div>
+  </StrictMode>
+);
---
+++ src/Cities.js
@@ -0,0 +1,16 @@
+import { Watch } from "react-loader-spinner";
+import Form from "./Form";
+export default function Cities() {
+  return (
+    <Watch
+      height="80"
+      width="80"
+      radius="48"
+      color="white"
+      ariable="watch-loading"
+      wrapperStyle={{}}
+      wrapperClassName=""
+      vissible="{true}"
+    />
+  );
+}
